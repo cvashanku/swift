dbf8fc91222aa2440de94d24ce15f009f8b66162
cvashanku <cvashankar18@gmail.com>
2023-03-07 22:53:30 +0530

    programs on functions in swift

---
+++ functions/prog10.swift
@@ -0,0 +1,21 @@
+//function overloading
+import Foundation
+func addition(a:Int,b:Int)
+{
+    
+}
+func addition(b:Int,c:Int){
+    
+}
+func addition(b:Int,a:Int){
+    
+}
+func addition(a:Int,b:Float){
+    
+}
+func addition(a:Float,b:Int){
+    
+}
+func addition(a:Int,b:Int,c:Int){
+    
+}
